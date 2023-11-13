8dbbeb8c61669afef3c0fb600862e7da4e8ccbaf
agnesyys <agnesyauys@gmail.com>
2023-11-13 10:01:37 -0800

    implemented OpenNoteOutputData.java

--- outline/src/use_case/OpenNoteOutputData.java
+++ outline/src/use_case/OpenNoteOutputData.java
@@ -1,4 +1,19 @@
 package use_case;
 
 public class OpenNoteOutputData {
+    private String noteId;
+    private String message;
+
+    public OpenNoteOutputData(String noteId, String message){
+        this.noteId = noteId;
+        this.message = message;
+    }
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
