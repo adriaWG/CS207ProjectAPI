ca8d7171a24f52181490b9a4d0ac30979a5a3deb
Janus <2518840295@qq.com>
2023-11-13 10:09:08 -0500

    a general idea of how main should look like

--- outline/src/view/ViewManager.java
+++ outline/src/view/ViewManager.java
@@ -1,4 +1,11 @@
 package view;
 
+import interface_adapter.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class ViewManager {
+    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
+    }
 }
