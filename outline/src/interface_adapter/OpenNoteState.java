307a415e2355acc4c470fb3c45fd54aced20a155
agnesyys <agnesyauys@gmail.com>
2023-11-13 10:45:15 -0800

    implemented OpenNoteState.java

--- outline/src/interface_adapter/OpenNoteState.java
+++ outline/src/interface_adapter/OpenNoteState.java
@@ -1,4 +1,28 @@
 package interface_adapter;
 
 public class OpenNoteState {
+    public boolean isAuthenticated;
+    private String errorMessage;
+
+    public OpenNoteState(){
+        // initialise the state
+        isAuthenticated = false;
+        errorMessage = null;
+    }
+
+    public boolean isAuthenticated(){
+        return isAuthenticated;
+    }
+
+    public void setAuthenticated(boolean isAuthenticated){
+        this.isAuthenticated = isAuthenticated;
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
 }
