af99108a05d2caf1555cd895980e38272e36e143
agnesyys <agnesyauys@gmail.com>
2023-11-13 09:57:53 -0800

    implemented OpenNoteInputData.java

--- outline/src/use_case/OpenNoteInputData.java
+++ outline/src/use_case/OpenNoteInputData.java
@@ -1,4 +1,20 @@
 package use_case;
 
 public class OpenNoteInputData {
+    private String title;
+    private String content;
+
+    public OpenNoteInputData(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }
 }
+
