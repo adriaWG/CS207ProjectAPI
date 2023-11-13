7a35fb6bd47321829213e47714cbe0ecf96cf94b
emilytieu <emily.tieu@mail.utoronto.ca>
2023-11-13 09:50:20 -0500

    Revert "Translate comments"
    This reverts commit 25487c88663a9cf879ad9cd3934192a8d0ffdf81.

--- notetakingapi/src/main/java/tryAPI.java
+++ notetakingapi/src/main/java/tryAPI.java
@@ -7,22 +7,22 @@ import org.apache.http.util.EntityUtils;
 
 public class tryAPI{
     public static void main(String[] args) throws Exception {
-        // 1. Get access token
+        // 1. 获取访问令牌
         String accessToken = "ACCESS_TOKEN";
 
-        // 2. Build an API request
+        // 2. 构建 API 请求
         String apiUrl = "https://graph.microsoft.com/v1.0/me/notes";
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(apiUrl);
         httpGet.setHeader("Authorization", "Bearer " + accessToken);
 
-        // 3. Send API request and parse the response
+        // 3. 发送 API 请求并解析响应
         HttpResponse response = httpClient.execute(httpGet);
         HttpEntity entity = response.getEntity();
         String responseJson = EntityUtils.toString(entity);
 
-        // Parse the response
-        // TODO: Extract the required information based on the response JSON data
+        // 解析响应
+        // TODO: 根据响应的 JSON 数据提取所需的信息
         System.out.println(responseJson);
     }
 
