632f9b73b5bf9a4892828d073d3b9fffaec25ac6
adria_Wg <20307130377@fudan.edu.cn>
2023-11-18 15:07:11 -0500

    add ViewMode in interface_adapter

--- outline/src/interface_adapter/OutNote_adapter/OutNoteViewModel.java
+++ outline/src/interface_adapter/OutNote_adapter/OutNoteViewModel.java
@@ -1,12 +1,18 @@
 package interface_adapter.OutNote_adapter;
 
+import interface_adapter.ViewModel;
+
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-public class OutNoteViewModel {
+public class OutNoteViewModel extends ViewModel {
     public final String TITLE_LABEL = "OutNote View";
 
     private static OutNoteState state = new OutNoteState();
+
+    public OutNoteViewModel(String viewName) {
+        super(viewName);
+    }
 
     public void setState(OutNoteState state) {
         this.state = state;
