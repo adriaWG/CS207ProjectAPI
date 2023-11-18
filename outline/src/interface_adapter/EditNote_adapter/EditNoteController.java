b0b746bccdfc5e4c99fb469429f6dd579c2f68ce
Janus <2518840295@qq.com>
2023-11-18 15:17:45 -0500

    EditNote_adapter changes

--- outline/src/interface_adapter/EditNote_adapter/EditNoteController.java
+++ outline/src/interface_adapter/EditNote_adapter/EditNoteController.java
@@ -1,4 +1,21 @@
 package interface_adapter.EditNote_adapter;
 
+import use_case.EditNote_case.EditNoteInputBoundary;
+import use_case.EditNote_case.EditNoteInputData;
+
 public class EditNoteController {
+    final EditNoteInputBoundary editUseCaseInteractor;
+    public EditNoteController(EditNoteInputBoundary editUseCaseInteractor) {
+        this.editUseCaseInteractor = editUseCaseInteractor;
+    }
+
+    public void execute(String filename) {
+        EditNoteInputData editNoteInputData = new EditNoteInputData(
+                filename);
+
+        editUseCaseInteractor.edit(editNoteInputData);
+    }
+
 }
+
+
--- outline/src/interface_adapter/EditNote_adapter/EditNoteState.java
+++ outline/src/interface_adapter/EditNote_adapter/EditNoteState.java
@@ -1,4 +1,23 @@
 package interface_adapter.EditNote_adapter;
 
 public class EditNoteState {
+    private String filename = "";
+    private String filenameError = null;
+    public EditNoteState(EditNoteState copy) {
+        filename = copy.filename;
+        filenameError = copy.filenameError;
+
+    }
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public EditNoteState() {}
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public String getFilenameError() {
+        return filenameError;
+    }
+
+
 }
