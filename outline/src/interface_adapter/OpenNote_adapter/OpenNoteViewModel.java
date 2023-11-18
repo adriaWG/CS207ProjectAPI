abd17e3ffcd6fe87bec1ae5992c6dc405da5fd35
agnesyys <agnesyauys@gmail.com>
2023-11-18 14:09:27 -0800

    implemented OpenNoteViewModel.java

--- outline/src/interface_adapter/OpenNote_adapter/OpenNoteViewModel.java
+++ outline/src/interface_adapter/OpenNote_adapter/OpenNoteViewModel.java
@@ -1,4 +1,20 @@
 package interface_adapter.OpenNote_adapter;
 
+import app.OpenNoteUseCaseFactory;
+import use_case.OpenNote_case.OpenNoteInputData;
+
 public class OpenNoteViewModel {
+    private final OpenNoteUseCaseFactory useCase;
+    private final view.OpenNoteView view;
+
+    public OpenNoteViewModel(OpenNoteUseCaseFactory useCase, view.OpenNoteView view){
+        this.useCase =useCase;
+        this.view = view;
+    }
+
+    public void createNote(){
+        OpenNoteInputData inputData = view.collectNoteInput();
+
+        useCase.createNote(inputData);
+    }
 }
