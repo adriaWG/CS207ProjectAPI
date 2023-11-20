3911224c29ae68a3feb87864cb57322089edd8fd
emilytieu <emily.tieu@mail.utoronto.ca>
2023-11-20 10:35:54 -0500

    clear controller use case factory

--- outline/src/view/EditNoteView.java
+++ outline/src/view/EditNoteView.java
@@ -1,31 +1,16 @@
 package view;
 
-import use_case.ClearNote_case.ClearNoteInputBoundary;
-
-import use_case.ClearNote_case.ClearNoteInterface;
-import use_case.ClearNote_case.ClearNoteOutputBoundary;
-import use_case.ClearNote_case.ClearNoteUserDataAccessInterface;
+import use_case.ClearNote.ClearNoteInputBoundary;
+import use_case.ClearNote.ClearNoteInterface;
+import use_case.ClearNote.ClearNoteOutputBoundary;
+import use_case.ClearNote.ClearNoteUserDataAccessInterface;
 
 public class EditNoteView {
-//    ClearNoteUserDataAccessInterface dataAccess = new ClearNoteUserDataAccess();
-//    ClearNoteInputBoundary inputBoundary = new ClearNoteInputData(dataAccess);
-//    ClearNoteOutputBoundary outputBoundary = new ClearNoteOutputData();
-//    ClearNoteInterface interactor = new ClearNoteInteractor(inputBoundary);
-//
-//// To delete a notebook
-//interactor.deleteNotebook("yourNotebookId", outputBoundary);
-}
+    ClearNoteUserDataAccessInterface dataAccess = new ClearNoteUserDataAccess();
+    ClearNoteInputBoundary inputBoundary = new ClearNoteInputData(dataAccess);
+    ClearNoteOutputBoundary outputBoundary = new ClearNoteOutputData();
+    ClearNoteInterface interactor = new ClearNoteInteractor(inputBoundary);
 
-
-
-
-
-
-
-
-
-
-
-
-
+// To delete a notebook
+interactor.deleteNotebook("yourNotebookId", outputBoundary);
 }
