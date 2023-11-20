fa179edeeb13337fc5cc66d3cac7c4909e6ff9cd
agnesyys <agnesyauys@gmail.com>
2023-11-20 10:28:42 -0800

    edited OpenNoteView.java

--- outline/src/view/OpenNoteView.java
+++ outline/src/view/OpenNoteView.java
@@ -117,22 +117,3 @@ public class OpenNoteView extends JPanel implements ActionListener, PropertyChan
 
 }
 
-//public class OpenNoteView implements OpenNoteOutputBoundary {
-//    private final Scanner scanner;
-//    public OpenNoteView(){
-//        this.scanner = new Scanner(System.in);
-//    }
-//
-//    public void presentNoteCreated(OpenNoteOutputData outputData){
-//        System.out.println("Note ID:" + outputData.getNoteId());
-//        System.out.println(outputData.getMessage());
-//    }
-//
-//    public OpenNoteInputData collectNoteInput(){
-//        System.out.println("Enter note title:");
-//        String title = scanner.nextLine();
-//        System.out.println("Enter note content:");
-//        String content = scanner.nextLine();
-//
-//        return new OpenNoteInputData(title, content);
-//    }
