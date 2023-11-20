a8f5989ac06ddb0807d639c424a82931df869e3c
emilytieu <emily.tieu@mail.utoronto.ca>
2023-11-20 09:54:00 -0500

    ClearNote State

--- outline/src/interface_adapter/ClearNote_adapter/ClearNoteState.java
+++ outline/src/interface_adapter/ClearNote_adapter/ClearNoteState.java
@@ -1,9 +1,29 @@
 package interface_adapter.ClearNote_adapter;
 
 public class ClearNoteState {
-    public void setFilename(String filename) {
+    private String filename = "";
+    private String filenameError = null;
+
+    public ClearNoteState(ClearNoteState copy) {
+        filename = copy.filename;
+        filenameError = copy.filenameError;
     }
 
-    public void setFilenameError(String error) {
+    public ClearNoteState() {}
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilenameError(String filename) {
+        this.filename = filenameError;
+    }
+
+    public String getFilenameError() {
+        return filenameError;
     }
 }
