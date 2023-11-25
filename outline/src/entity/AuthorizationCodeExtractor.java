f5fb11a691175c48d159834a0e961b676e131e9e
adria_Wg <20307130377@fudan.edu.cn>
2023-11-25 14:26:57 -0500

    extract authorizationcode

---
+++ outline/src/entity/AuthorizationCodeExtractor.java
@@ -0,0 +1,28 @@
+package entity;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class AuthorizationCodeExtractor {
+    public static String extract(String url) {
+
+        try {
+            URI uri = new URI(url);
+            String query = uri.getQuery();
+            String[] params = query.split("&");
+            String code = null;
+
+            for (String param : params) {
+                if (param.startsWith("code=")) {
+                    code = param.substring(5);
+                    break;
+                }
+            }
+
+            return code;
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
