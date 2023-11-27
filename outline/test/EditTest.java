5514e1c8d682808d73c3bb672224fbde4fba2e49
emilytieu <emily.tieu@mail.utoronto.ca>
2023-11-27 09:59:06 -0500

    test if main runs + fixed small OpenTest bug so that the tests run now

--- outline/test/EditTest.java
+++ outline/test/EditTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import app.Main;
+import org.junit.Test;
 import use_case.EditNote_case.EditNoteInputBoundary;
 import interface_adapter.EditNote_adapter.EditNoteController;
 import interface_adapter.EditNote_adapter.EditNoteViewModel;
@@ -7,5 +10,9 @@ public class EditTest {
     @org.junit.Test
     public void testEditNote(){
 
+
     }
+
+    @Test
+    public void testMainRuns() {Main.main(new String[]{});}
 }
