3dc3f8ed13cca150cf19a1793902fc84787802ab
agnesyys <agnesyauys@gmail.com>
2023-12-02 17:21:04 -0800

    added testing files

---
+++ outline/test/app/EditNoteUseCaseFactoryTest.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EditNoteUseCaseFactoryTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void createEditNoteUseCase() {
+    }
+}
